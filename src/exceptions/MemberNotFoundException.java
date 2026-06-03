9dd34f777a3fdb125eff1de3ca8c7d90727b44ab
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2026-06-03 10:46:26 +0530

    Excpetion Strategies Implemented , With Custom Exception Messages

---
+++ src/exceptions/MemberNotFoundException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class MemberNotFoundException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+}
